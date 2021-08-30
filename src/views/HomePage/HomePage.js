1f67792c42431da487896fded60b3040e9a3f26d
MadsHaerup <69457070+MadsHaerup@users.noreply.github.com>
2021-08-30 12:45:10 +0200

    homepage component

---
+++ src/views/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Directory from '../../components/Directory/Directory';
+import './HomePage.scss';
+
+export const HomePage = () => {
+  return (
+    <div className="homepage">
+     <Directory />
+    </div>
+  )
+}
+
